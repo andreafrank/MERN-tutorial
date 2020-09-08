58f6ef534131c08f17ee2a7e9dd714455d69957e
andreafrank <andreajfrank23@gmail.com>
2020-09-07 22:26:32 -0500

    add exercise model

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -1,1 +1,14 @@
-exercise.model.js
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+	username: { type: String, required: true },
+	description: { type: String, required: true },
+	duration: { type: Number, required: true },
+	date: { type: Date, required: true }
+}, {
+	timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
