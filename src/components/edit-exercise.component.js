d6909fb2bfd36793e25bcec2a6379be443c785bc
andreafrank <andreajfrank23@gmail.com>
2020-09-11 23:11:38 -0500

    add user, create exercise

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -1,1 +1,11 @@
-edit-exercise.component.js
+import React, { Component } from 'react';
+
+export default class EditExercises extends Component {
+	render() {
+		return (
+			<div>
+				<p>You are on the edit exercises component.</p>
+			</div>
+		)
+	}
+}
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,1 +1,11 @@
-exercises-list.component.js
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+	render() {
+		return (
+			<div>
+				<p>You are on the exercises list component.</p>
+			</div>
+		)
+	}
+}
